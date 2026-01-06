1e5681f65f30884791265b4036f7a894a5c46537
Colten Parker <ColtenParker00@gmail.com>
2026-01-06 13:05:42 -0500

    Created initial react app front end

--- api/src/app.js
+++ api/src/app.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import Database from 'better-sqlite3';
 import favorites from './routes/favorites.js';
+import cors from 'cors';
+
 const db = new Database('favorites.db');
 
 const app = express();
-app.use(express.json());
 const port = 3000;
+app.use(express.json());
+//app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE'
+    );
+    next();
+});
 
 app.use('/favorites', favorites);
 
---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+const App = () => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/favorites/');
+                const data = await response.json();
+                setData(data);
+            } catch (err) {
+                setError('Error fetching data: ' + err.message);
+            }
+        };
+
+        fetchData();
+
+        console.log('use Effect');
+    }, []);
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    if (!data) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            {data.favorites.map((fav) => {
+                console.log(fav);
+                return (
+                    <div key={fav.id}>
+                        <a href={fav.url}>{fav.name}</a>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default App;
