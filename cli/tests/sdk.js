fef81bd234342d06b1745adb499a810ee914fff9
Colten Parker <ColtenParker00@gmail.com>
2026-01-06 10:52:25 -0500

    Created SDK for API

---
+++ cli/tests/sdk.js
@@ -0,0 +1,26 @@
+//Functions imported individually to keep track of tests
+
+import {
+    addFavorite,
+    deleteFavorite,
+    getFavorite,
+    getFavorites,
+    replaceFavorite,
+} from '../src/lib/sdk.js';
+
+const newFavId = await addFavorite('example', 'example');
+console.log(newFavId);
+
+let result = await getFavorite(newFavId);
+console.log(result);
+
+result.name = 'test-new';
+await replaceFavorite(result.id, result);
+
+const fav = await getFavorite(result.id);
+console.log('updated:', fav);
+
+await deleteFavorite(newFavId);
+
+result = await getFavorite(newFavId);
+console.log(result);
